29c66e2d35bfe703cce815e405fb1dff9e8cec36
Anshul Singh Chauhan <anshulchauhanworks@gmail.com>
2025-06-12 15:46:52 +0530

    about added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import HoverCards from './components/Card';
-import ICTPage from './pages/ICTPage';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import HoverCards from "./components/Card";
+import ICTPage from "./pages/ICTPage";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      
+
       <Routes>
         <Route path="/" element={<HoverCards />} />
         <Route path="/schools/ict" element={<ICTPage />} />
         {/* Add more <Route> entries here for other schools if needed */}
       </Routes>
-      
+
       <Footer />
     </Router>
   );
